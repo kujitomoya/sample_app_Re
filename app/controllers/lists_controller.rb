e2250cc54823488a590a4c57e479294107bd14a0
EC2 Default User <ec2-user@ip-172-31-0-5.ap-northeast-1.compute.internal>
2023-05-16 14:40:40 +0000

    [Finish_4syou]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,13 +6,17 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    # redirect_to '/top' を削除して、以下コードに変更
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
